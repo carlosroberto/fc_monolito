259f2c0897844d7ba87b1bb5a8fcebe5e982a9eb
Carlos Junior <carlosroberto366@hotmail.com>
2022-12-05 22:45:24 -0300

    implementando facade

---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,30 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import ProductAdmFacadeInterface, { AddProductFacadeInputDto, CheckStockFacadeInputDto, CheckStockFacadeOutputDto } from "./product-adm.facade.interface";
+
+
+export interface UseCasesProps {
+    addUseCase: UseCaseInterface
+    stockUseCase: UseCaseInterface
+
+}
+
+export default  class ProductAdmFacade implements ProductAdmFacadeInterface{
+
+    private _addUseCase: UseCaseInterface
+    private _checkStockUseCase: UseCaseInterface
+
+    
+    constructor(usecasesProps: UseCasesProps){
+        this._addUseCase = usecasesProps.addUseCase
+        this._checkStockUseCase = usecasesProps.stockUseCase
+    }
+
+    addProduct(input: AddProductFacadeInputDto): Promise<void> {
+        //nesse caso o nome do dto foi igual, mas caso o dto do caso de uso for diferente do deto da facade, converter o dto da facade par ao dto do caso de uso.
+        return this._addUseCase.execute(input)
+    }
+    checkStock(input: CheckStockFacadeInputDto): Promise<CheckStockFacadeOutputDto> {
+        return this._checkStockUseCase.execute(input)
+    }
+    
+}
